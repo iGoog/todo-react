e8b98a25d96d116511f7417aede4715a65c15b2d
iGoog <paul@picayune.co>
2020-09-21 13:34:55 -0700

    removed test and made todo app.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const keyGenerator = useRef(0);
+    const [todoList, setTodoList] = useState([]);
+    const [todoItem, setTodoItem] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setTodoList(todoList.concat([
+            {key: keyGenerator.current++, value: e.target.todoItem.value}
+            ]));
+        setTodoItem("");
+    }
+
+    const handleDelete = (e, key) => {
+        e.preventDefault();
+        setTodoList(todoList.filter((el)=>el.key!==key));
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+          <label>
+              Todo Item:
+              <input
+                  type="text"
+                  name="todoItem"
+                  value={todoItem}
+                  onChange={e => setTodoItem(e.target.value)}
+              />
+              <ul>
+                  {todoList.map((todoListItem)=>
+                      <li key={todoListItem.key}>
+                          {todoListItem.value}
+                          {" "}
+                          <a
+                              role="button"
+                              href={"#"+todoListItem.key}
+                              onClick={(e)=> handleDelete(e, todoListItem.key)}
+                          >x</a>
+                      </li>
+                  )}
+              </ul>
+          </label>
+      </form>
     </div>
   );
 }
